a77d62ced2c1736c40d68fb3622202739c5ac84a
Mackensie Alvarez <mackensiealvarez@gmail.com>
2021-11-07 21:21:30 -0500

    Update user seeder to include notes

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Note;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -15,16 +16,19 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::factory()
+        ->has(Note::factory()->count(10))
         ->create([
             'email' => 'user1@test.com'
         ]);
 
         User::factory()
+        ->has(Note::factory()->count(3))
         ->create([
             'email' => 'user2@test.com'
         ]);
 
         User::factory()
+        ->has(Note::factory()->count(5))
         ->create([
             'email' => 'user3@test.com'
         ]);
